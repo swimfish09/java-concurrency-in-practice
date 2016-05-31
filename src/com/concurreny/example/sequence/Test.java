831beec56aa697d6f601f685f94425f3caf097f9
bruceliangsibin <bruceliangsibin@vip.qq.com>
2016-05-31 19:51:50 +0800

    edit sequence class

--- src/com/concurreny/example/sequence/Test.java
+++ src/com/concurreny/example/sequence/Test.java
@@ -1,33 +1,25 @@
 package com.concurreny.example.sequence;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-
 public class Test implements Runnable {
-	//private Sequence sequence;
-	private UnsafeSequence sequence;
+	private Sequence sequence;
+	// private UnsafeSequence sequence;
 
-	public Test(UnsafeSequence sequence) {
+	public Test(Sequence sequence) {
 		super();
 		this.sequence = sequence;
 	}
 
 	@Override
 	public void run() {
-		// UnsafeSequence sequence = new UnsafeSequence();
 		System.out.print(sequence.getValue() + " ");
 	}
 
 	public static void main(String[] args) throws InterruptedException {
-		ExecutorService service = Executors.newFixedThreadPool(10);
-		//Sequence sequence = new Sequence();
-		UnsafeSequence sequence = new UnsafeSequence();
+		Sequence sequence = new Sequence();
+		// UnsafeSequence sequence = new UnsafeSequence();
 		for (int i = 0; i < 20; i++) {
-			service.submit(new Test(sequence));
+			Thread thread = new Thread(new Test(sequence));
+			thread.start();
 		}
-		service.shutdown();
-		service.awaitTermination(Long.MAX_VALUE, TimeUnit.HOURS);
-		System.out.println("over...");
 	}
 }
